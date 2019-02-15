fbfd43b26c32ec5566addcca021ff655a1320c36
Christina Williams <christinaxtwilliams@gmail.com>
2019-02-15 01:00:36 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,3 +4,8 @@ Class Application
     resp = Rack::Response.new 
     req = Rack::Response.new(env)
     
+    if req.path.match(/items/)
+      item = req.path.split("/items/").last 
+      if item_object = @@items.detect {|i| i.name == item}
+        resp.write "#{item_object.name}: $#{item_object.price}"
+        
