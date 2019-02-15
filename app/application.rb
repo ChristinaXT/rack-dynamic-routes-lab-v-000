d86a7e04cbab84efbf3f59d6a66429ac2c322c99
Christina Williams <christinaxtwilliams@gmail.com>
2019-02-15 01:01:37 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,4 +8,10 @@ Class Application
       item = req.path.split("/items/").last 
       if item_object = @@items.detect {|i| i.name == item}
         resp.write "#{item_object.name}: $#{item_object.price}"
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
+    else
+      
         
